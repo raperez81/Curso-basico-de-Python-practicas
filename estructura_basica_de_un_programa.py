d00e106fe73b0e478b590017f4fd0ca6fa5a8bb8
Ricardo Perez <raperez81@hotmail.com>
2021-01-26 10:32:46 -0600

    Estructura basica de un programa en python

---
+++ estructura_basica_de_un_programa.py
@@ -0,0 +1,8 @@
+# Estructura basica de un programa de Python
+# Es buena práctica dejar dos enter entre la definicion de una funcion
+ def run():
+    pass
+
+
+if __name__ == '__main__':
+    run() 
