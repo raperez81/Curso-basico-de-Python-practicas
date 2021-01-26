2e13332bf0cb5a3a0f805f9baa92f2c588416970
Ricardo Perez <raperez81@hotmail.com>
2021-01-26 10:41:27 -0600

    Agregando palindromo.py

---
+++ palindromo.py
@@ -0,0 +1,24 @@
+# Un palindromo es una palabra, frase o parrafo que se lee igual de 
+# izquierda a derecha que de derecha a izquierda, este programa evalua
+# si una palabra o frase ingresada por el usuario es un palindromos
+def palindromo(palabra):
+    palabra = palabra.replace(' ', '')
+    palabra = palabra.lower()
+    palabra_invertida = palabra[::-1]
+    if palabra == palabra_invertida:
+        return True
+    else:
+        return False
+
+
+def run():
+    palabra = input('Escribe una palabra: ')
+    es_palindromo = palindromo(palabra)
+    if es_palindromo:
+        print('Es palindromo')
+    else:
+        print('No es palindromo')
+
+
+if __name__ == '__main__':
+    run()
